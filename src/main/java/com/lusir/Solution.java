26cb8650eab0757db8f5753249dc5587bbd660d4
vision fox <1548936488@qq.com>
2022-03-06 23:30:49 +0800

    train 03 06

--- src/main/java/com/lusir/Solution.java
+++ src/main/java/com/lusir/Solution.java
@@ -1523,4 +1523,19 @@ public class Solution {
         }
         return null;
     }
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+
+        for (int p : pushed) {
+            stack.push(p);
+            while (!stack.isEmpty()&&stack.peek()==popped[i]){
+                stack.pop();
+                i++;
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
